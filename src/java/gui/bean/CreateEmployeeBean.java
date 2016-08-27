7382b8a3d11e7b2660dbc7b93966742a8ca40b34
afacunaa <afacunaa@unal.edu.co>
2016-08-26 23:22:51 -0500

    Todos los paquetes y algunos archivos java vacios

---
+++ src/java/gui/bean/CreateEmployeeBean.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui.bean;
+
+import java.util.Date;
+
+/**
+ *
+ * @author afacunaa
+ */
+public class CreateEmployeeBean {
+    private Integer employeeId;
+    private String name;
+    private String lastName;
+    private Integer documentId;
+    private Date birthDate;
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(Integer documentId) {
+        this.documentId = documentId;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+    
+    public void createEmployee(){
+        
+    }
+}
